eedf4c23efdb21d500e5ba72b1cf5b0338339a13
juanalonso21 <juanalonsopulpi@gmail.com>
2025-02-10 18:24:33 +0100

    Portafolio con Spring y tailwindcss

---
+++ src/main/java/com/juanalonso/portfoliospring/PortfoliospringApplication.java
@@ -0,0 +1,13 @@
+package com.juanalonso.portfoliospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliospringApplication.class, args);
+	}
+
+}
